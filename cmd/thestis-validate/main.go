8253cbb2485b93e4c0e261f402268af80cbea179
djerys <djerys@yandex-team.ru>
2021-11-24 13:08:48 +0300

    THSIS-4: add default specification to validate

--- cmd/thestis-validate/main.go
+++ cmd/thestis-validate/main.go
@@ -8,9 +8,15 @@ import (
 	"github.com/harpyd/thestis/internal/adapter/parser/yaml"
 )
 
+const exampleSpecification = "./examples/specification/horns-and-hooves-test.yml"
+
 func main() {
 	flag.Parse()
+
 	specFilename := flag.Arg(0)
+	if specFilename == "" {
+		specFilename = exampleSpecification
+	}
 
 	specFile, err := os.Open(specFilename)
 	if err != nil {
