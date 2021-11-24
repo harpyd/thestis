78f13a24b24f2b4192a5e4050356f6b455f857fb
djerys <djerys@yandex-team.ru>
2021-11-24 13:04:23 +0300

    THSIS-4: make yaml parsing more complex

--- internal/adapter/parser/yaml/type.go
+++ internal/adapter/parser/yaml/type.go
@@ -1,7 +1,55 @@
 package yaml
 
-type specificationSchema struct {
-	Author      string `yaml:"author"`
-	Title       string `yaml:"title"`
-	Description string `yaml:"description"`
-}
+type (
+	specificationSchema struct {
+		Author      string                 `yaml:"author"`
+		Title       string                 `yaml:"title"`
+		Description string                 `yaml:"description"`
+		Stories     map[string]storySchema `yaml:"stories"`
+	}
+
+	storySchema struct {
+		Description string                    `yaml:"description"`
+		AsA         string                    `yaml:"asA"`
+		InOrderTo   string                    `yaml:"inOrderTo"`
+		WantTo      string                    `yaml:"wantTo"`
+		Scenarios   map[string]scenarioSchema `yaml:"scenarios"`
+	}
+
+	scenarioSchema struct {
+		Description string                  `yaml:"description"`
+		Theses      map[string]thesisSchema `yaml:"theses"`
+	}
+
+	thesisSchema struct {
+		Given     string          `yaml:"given"`
+		When      string          `yaml:"when"`
+		Then      string          `yaml:"then"`
+		After     []string        `yaml:"after"`
+		HTTP      httpSchema      `yaml:"http"`
+		Assertion assertionSchema `yaml:"assertion"`
+	}
+
+	httpSchema struct {
+		Method   string             `yaml:"method"`
+		URL      string             `yaml:"url"`
+		Request  httpRequestSchema  `yaml:"request"`
+		Response httpResponseSchema `yaml:"response"`
+	}
+
+	httpRequestSchema struct {
+		ContentType string                 `yaml:"contentType"`
+		Body        map[string]interface{} `yaml:"body"`
+	}
+
+	httpResponseSchema struct {
+		AllowedCodes       []int  `yaml:"allowedCodes"`
+		AllowedContentType string `yaml:"allowedContentType"`
+	}
+
+	assertionSchema struct {
+		Type   string                 `yaml:"type"`
+		Method string                 `yaml:"with"`
+		Assert map[string]interface{} `yaml:"assert"`
+	}
+)
