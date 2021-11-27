086ecbac2c2174b95b51db2995368a633f310b5f
djerys <kotukhov99@gmail.com>
2021-11-27 22:26:02 +0300

    THSIS-4: make map and slice coping more complex

--- pkg/deepcopy/map.go
+++ pkg/deepcopy/map.go
@@ -4,12 +4,21 @@ func StringInterfaceMap(m map[string]interface{}) map[string]interface{} {
 	copied := make(map[string]interface{}, len(m))
 
 	for k, v := range m {
-		cv, ok := v.(map[string]interface{})
-		if ok {
-			copied[k] = StringInterfaceMap(cv)
-		} else {
-			copied[k] = v
+		mapVal, isMap := v.(map[string]interface{})
+		if isMap {
+			copied[k] = StringInterfaceMap(mapVal)
+
+			continue
 		}
+
+		sliceVal, isSlice := v.([]interface{})
+		if isSlice {
+			copied[k] = InterfaceSlice(sliceVal)
+
+			continue
+		}
+
+		copied[k] = v
 	}
 
 	return copied
--- pkg/deepcopy/slice.go
+++ pkg/deepcopy/slice.go
@@ -6,3 +6,27 @@ func IntSlice(s []int) []int {
 
 	return copied
 }
+
+func InterfaceSlice(s []interface{}) []interface{} {
+	copied := make([]interface{}, 0, len(s))
+
+	for _, v := range s {
+		mapVal, isMap := v.(map[string]interface{})
+		if isMap {
+			copied = append(copied, StringInterfaceMap(mapVal))
+
+			continue
+		}
+
+		sliceVal, isSlice := v.([]interface{})
+		if isSlice {
+			copied = append(copied, InterfaceSlice(sliceVal))
+
+			continue
+		}
+
+		copied = append(copied, v)
+	}
+
+	return copied
+}
