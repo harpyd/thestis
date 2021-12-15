4263a08aec0ea92fd1524f2273d32a50d813bd3d
djerys <kotukhov99@gmail.com>
2021-12-15 13:20:34 +0300

    THSIS-11: add necessary repositories methods

--- internal/app/command/repository.go
+++ internal/app/command/repository.go
@@ -3,11 +3,24 @@ package command
 import (
 	"context"
 
+	"github.com/harpyd/thestis/internal/domain/specification"
 	"github.com/harpyd/thestis/internal/domain/testcampaign"
 )
 
 type (
 	testCampaignsRepository interface {
+		GetTestCampaign(ctx context.Context, tcID string) (*testcampaign.TestCampaign, error)
 		AddTestCampaign(ctx context.Context, tc *testcampaign.TestCampaign) error
+		UpdateTestCampaign(ctx context.Context, tcID string, updateFn TestCampaignUpdater) error
 	}
+
+	TestCampaignUpdater func(
+		ctx context.Context,
+		tc *testcampaign.TestCampaign,
+	) (*testcampaign.TestCampaign, error)
 )
+
+type specificationRepository interface {
+	GetSpecification(ctx context.Context, specID string) (*specification.Specification, error)
+	AddSpecification(ctx context.Context, spec *specification.Specification) error
+}
