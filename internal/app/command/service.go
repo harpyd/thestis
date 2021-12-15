2b1f7c6a335877aea3da5d9088b8f64e98343ec1
djerys <kotukhov99@gmail.com>
2021-12-15 19:00:43 +0300

    THSIS-11: remove useless //nolint comment

--- internal/app/command/service.go
+++ internal/app/command/service.go
@@ -6,7 +6,6 @@ import (
 	"github.com/harpyd/thestis/internal/domain/specification"
 )
 
-// nolint
 type specificationParserService interface {
 	ParseSpecification(specID string, reader io.Reader) (*specification.Specification, error)
 }
