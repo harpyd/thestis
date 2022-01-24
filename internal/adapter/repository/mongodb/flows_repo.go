b1aa056533c55bc481366a69e8fa021963026085
djerys <kotukhov99@gmail.com>
2022-01-25 00:35:51 +0300

    Add Mongo DB flows repository (#47)

---
+++ internal/adapter/repository/mongodb/flows_repo.go
@@ -0,0 +1,67 @@
+package mongodb
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/harpyd/thestis/internal/app"
+	"github.com/harpyd/thestis/internal/domain/performance"
+)
+
+type FlowsRepository struct {
+	flows *mongo.Collection
+}
+
+const flows = "flows"
+
+func NewFlowsRepository(db *mongo.Database) *FlowsRepository {
+	return &FlowsRepository{
+		flows: db.Collection(flows),
+	}
+}
+
+func (r *FlowsRepository) GetFlow(ctx context.Context, flowID string) (performance.Flow, error) {
+	document, err := r.getFlowDocument(ctx, flowID, "")
+	if err != nil {
+		return performance.Flow{}, err
+	}
+
+	return document.unmarshalToFlow(), err
+}
+
+func (r *FlowsRepository) getFlowDocument(ctx context.Context, flowID, userID string) (flowDocument, error) {
+	filter := makeFlowFilter(flowID, userID)
+
+	var document flowDocument
+	if err := r.flows.FindOne(ctx, filter).Decode(&document); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments); err != nil {
+			return flowDocument{}, app.NewFlowNotFoundError(err)
+		}
+
+		return flowDocument{}, app.NewDatabaseError(err)
+	}
+
+	return document, nil
+}
+
+func makeFlowFilter(flowID string, userID string) bson.M {
+	filter := bson.M{"_id": flowID}
+	if userID != "" {
+		filter["ownerId"] = userID
+	}
+
+	return filter
+}
+
+func (r *FlowsRepository) UpsertFlow(ctx context.Context, flow performance.Flow) error {
+	document := marshalToFlowDocument(flow)
+
+	opt := options.Update().SetUpsert(true)
+	_, err := r.flows.UpdateByID(ctx, flow.ID(), document, opt)
+
+	return app.NewDatabaseError(err)
+}
