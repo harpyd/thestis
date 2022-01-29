582159bf04f949141ae524dd5124cc10e6c083a3
djerys <kotukhov99@gmail.com>
2022-01-29 08:43:23 +0300

    Extract flow managing logic to FlowManager (#45)

---
+++ internal/app/flow_manager.go
@@ -0,0 +1,99 @@
+package app
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/harpyd/thestis/internal/domain/performance"
+)
+
+type (
+	PerformanceManager interface {
+		ManageFlow(ctx context.Context, perf *performance.Performance) (<-chan Message, error)
+	}
+
+	Message struct {
+		s string
+	}
+)
+
+func newMessageFromStringer(s fmt.Stringer) Message {
+	return Message{s: s.String()}
+}
+
+func newMessageFromError(err error) Message {
+	return Message{s: err.Error()}
+}
+
+func (m Message) String() string {
+	return m.s
+}
+
+type everyStepSavingFlowManager struct {
+	perfsRepo PerformancesRepository
+	flowsRepo FlowsRepository
+}
+
+func NewStepByStepSavingFlowManager(
+	perfsRepo PerformancesRepository,
+	flowsRepo FlowsRepository,
+) PerformanceManager {
+	if perfsRepo == nil {
+		panic("performance repository is nil")
+	}
+
+	if flowsRepo == nil {
+		panic("flows repository is nil")
+	}
+
+	return &everyStepSavingFlowManager{
+		perfsRepo: perfsRepo,
+		flowsRepo: flowsRepo,
+	}
+}
+
+func (m *everyStepSavingFlowManager) ManageFlow(
+	ctx context.Context,
+	perf *performance.Performance,
+) (<-chan Message, error) {
+	steps, err := perf.Start(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	msg := make(chan Message)
+
+	if err = m.perfsRepo.ExclusivelyDoWithPerformance(ctx, perf, m.action(ctx, steps, msg)); err != nil {
+		return nil, err
+	}
+
+	return msg, nil
+}
+
+func (m *everyStepSavingFlowManager) action(
+	ctx context.Context,
+	steps <-chan performance.Step,
+	msg chan<- Message,
+) func(perf *performance.Performance) {
+	return func(perf *performance.Performance) {
+		defer close(msg)
+
+		fr := performance.FlowFromPerformance(perf)
+
+		for s := range steps {
+			fr.WithStep(s)
+
+			flow := fr.Reduce()
+			if err := m.flowsRepo.UpsertFlow(ctx, flow); err != nil {
+				msg <- newMessageFromError(err)
+			}
+
+			msg <- newMessageFromStringer(s)
+		}
+
+		flow := fr.FinallyReduce()
+		if err := m.flowsRepo.UpsertFlow(ctx, flow); err != nil {
+			msg <- newMessageFromError(err)
+		}
+	}
+}
