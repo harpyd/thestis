2def08222080b0b278e07d9c6bda14620a765d0c
djerys <kotukhov99@gmail.com>
2022-01-30 17:48:25 +0300

    Add `unmarshalStartNewPerformanceCommand` (#45)

---
+++ internal/port/http/v1/performance_mapping.go
@@ -0,0 +1,23 @@
+package v1
+
+import (
+	"net/http"
+
+	"github.com/harpyd/thestis/internal/app"
+)
+
+// nolint
+func unmarshalStartNewPerformanceCommand(
+	w http.ResponseWriter, r *http.Request,
+	testCampaignID string,
+) (cmd app.StartNewPerformanceCommand, ok bool) {
+	user, ok := unmarshalUser(w, r)
+	if !ok {
+		return
+	}
+
+	return app.StartNewPerformanceCommand{
+		TestCampaignID: testCampaignID,
+		StartedByID:    user.UUID,
+	}, true
+}
