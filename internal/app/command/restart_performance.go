82d87a64d7c21de5c8e5c2aa2d92de08c8ed3b41
djerys <kotukhov99@gmail.com>
2022-02-02 23:14:59 +0300

    Add `RestartPerformanceHandler` (#52)

---
+++ internal/app/command/restart_performance.go
@@ -0,0 +1,64 @@
+package command
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	"github.com/harpyd/thestis/internal/app"
+	"github.com/harpyd/thestis/internal/domain/user"
+)
+
+type RestartPerformanceHandler struct {
+	perfsRepo     app.PerformancesRepository
+	manager       app.FlowManager
+	performerOpts app.PerformerOptions
+}
+
+func NewRestartPerformanceHandler(
+	perfsRepo app.PerformancesRepository,
+	manager app.FlowManager,
+	opts ...app.PerformerOption,
+) RestartPerformanceHandler {
+	if perfsRepo == nil {
+		panic("performances repository is nil")
+	}
+
+	if manager == nil {
+		panic("flow manager is nil")
+	}
+
+	return RestartPerformanceHandler{
+		perfsRepo:     perfsRepo,
+		manager:       manager,
+		performerOpts: opts,
+	}
+}
+
+func (h RestartPerformanceHandler) Handle(
+	ctx context.Context,
+	cmd app.RestartPerformanceCommand,
+) (messages <-chan app.Message, err error) {
+	defer func() {
+		err = errors.Wrap(err, "performance restarting")
+	}()
+
+	perf, err := h.perfsRepo.GetPerformance(ctx, cmd.PerformanceID)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = user.CanSeePerformance(cmd.StartedByID, perf); err != nil {
+		return nil, err
+	}
+
+	actionCtx := context.Background()
+
+	messages, err = h.manager.ManageFlow(actionCtx, perf)
+	if err != nil {
+		return nil, err
+
+	}
+
+	return messages, nil
+}
