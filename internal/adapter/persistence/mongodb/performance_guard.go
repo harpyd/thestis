24339c58aabdd5b6c8237fea87dc41589e16dc82
djerys <kotukhov99@gmail.com>
2022-02-20 19:33:54 +0300

    Add mongo `PerformanceGuard` (#55)

--- internal/adapter/persistence/mongodb/performance_guard.go
+++ internal/adapter/persistence/mongodb/performance_guard.go
@@ -2,7 +2,11 @@ package mongodb
 
 import (
 	"context"
+	"errors"
 
+	"github.com/harpyd/thestis/internal/app"
+	"github.com/harpyd/thestis/internal/domain/performance"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -17,9 +21,35 @@ func NewPerformanceGuard(db *mongo.Database) *PerformanceGuard {
 }
 
 func (g *PerformanceGuard) AcquirePerformance(ctx context.Context, perfID string) error {
+	var document performanceDocument
+
+	var (
+		filter = bson.M{"_id": perfID}
+		update = bson.M{"$set": bson.M{"locked": true}}
+	)
+
+	if err := g.performances.FindOneAndUpdate(ctx, filter, update).Decode(&document); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return app.NewPerformanceNotFoundError(err)
+		}
+
+		return app.NewDatabaseError(err)
+	}
+
+	if document.Locked {
+		return performance.NewAlreadyStartedError()
+	}
+
 	return nil
 }
 
 func (g *PerformanceGuard) ReleasePerformance(ctx context.Context, perfID string) error {
-	return nil
+	update := bson.M{"$set": bson.M{"locked": false}}
+
+	_, err := g.performances.UpdateByID(ctx, perfID, update)
+	if errors.Is(err, mongo.ErrNoDocuments) {
+		return app.NewPerformanceNotFoundError(err)
+	}
+
+	return app.NewDatabaseError(err)
 }
---
+++ internal/adapter/persistence/mongodb/performance_guard_test.go
@@ -0,0 +1,104 @@
+package mongodb_test
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	"github.com/harpyd/thestis/internal/adapter/persistence/mongodb"
+	"github.com/harpyd/thestis/internal/domain/performance"
+	"github.com/stretchr/testify/suite"
+)
+
+type PerformanceGuardTestSuite struct {
+	suite.Suite
+	MongoTestFixtures
+
+	guard *mongodb.PerformanceGuard
+	repo  *mongodb.PerformancesRepository
+
+	perfID string
+}
+
+func (s *PerformanceGuardTestSuite) SetupTest() {
+	s.guard = mongodb.NewPerformanceGuard(s.db)
+	s.repo = mongodb.NewPerformancesRepository(s.db)
+
+	s.perfID = "2db44433-7142-4080-bada-844afccfedbf"
+
+	err := s.repo.AddPerformance(context.Background(), performance.Unmarshal(performance.Params{
+		OwnerID:         "b4a232b3-d853-4022-9b4d-f2aefb24d82c",
+		SpecificationID: "e1cdd1fb-8e2d-4b46-9e21-10375794af61",
+	}, performance.WithID(s.perfID)))
+	s.Require().NoError(err)
+}
+
+func (s *PerformanceGuardTestSuite) TearDownTest() {
+	err := s.repo.RemoveAllPerformances(context.Background())
+	s.Require().NoError(err)
+}
+
+func TestPerformanceGuard(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Integration tests are skipped")
+	}
+
+	suite.Run(t, &PerformanceGuardTestSuite{
+		MongoTestFixtures: MongoTestFixtures{t: t},
+	})
+}
+
+func (s *PerformanceGuardTestSuite) TestAcquirePerformance() {
+	err := s.guard.AcquirePerformance(context.Background(), s.perfID)
+	s.Require().NoError(err)
+
+	err = s.guard.AcquirePerformance(context.Background(), s.perfID)
+	s.Require().True(performance.IsAlreadyStartedError(err))
+
+	persistedPerf, err := s.repo.GetPerformance(context.Background(), s.perfID)
+	s.Require().NoError(err)
+
+	s.Require().True(persistedPerf.Locked())
+}
+
+func (s *PerformanceGuardTestSuite) TestAcquirePerformance_many_tries() {
+	const triesCount = 100
+
+	var wg sync.WaitGroup
+
+	var acquiredCount int32 = 0
+
+	wg.Add(triesCount)
+	for i := 1; i <= triesCount; i++ {
+		go func() {
+			defer wg.Done()
+
+			if err := s.guard.AcquirePerformance(context.Background(), s.perfID); err == nil {
+				atomic.AddInt32(&acquiredCount, 1)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	s.Require().EqualValues(1, acquiredCount)
+}
+
+func (s *PerformanceGuardTestSuite) TestReleasePerformance() {
+	err := s.guard.AcquirePerformance(context.Background(), s.perfID)
+	s.Require().NoError(err)
+
+	persistedPerf, err := s.repo.GetPerformance(context.Background(), s.perfID)
+	s.Require().NoError(err)
+
+	s.Require().True(persistedPerf.Locked())
+
+	err = s.guard.ReleasePerformance(context.Background(), s.perfID)
+	s.Require().NoError(err)
+
+	persistedPerf, err = s.repo.GetPerformance(context.Background(), s.perfID)
+	s.Require().NoError(err)
+
+	s.Require().False(persistedPerf.Locked())
+}
