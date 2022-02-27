f2727f02d841d3bc5fd2f9e1da1af567c3d5a2af
djerys <kotukhov99@gmail.com>
2022-02-27 12:36:48 +0300

    Refactor `repository` errors tests (#63)

--- internal/app/repository_test.go
+++ internal/app/repository_test.go
@@ -9,23 +9,80 @@ import (
 	"github.com/harpyd/thestis/internal/app"
 )
 
-func TestIsDatabaseError(t *testing.T) {
+func TestRepositoryErrors(t *testing.T) {
 	t.Parallel()
 
 	testCases := []struct {
-		Name      string
-		Err       error
-		IsSameErr bool
+		Name     string
+		Err      error
+		IsErr    func(err error) bool
+		Reversed bool
 	}{
 		{
-			Name:      "database_error",
-			Err:       app.NewDatabaseError(errors.New("failed to connect")),
-			IsSameErr: true,
+			Name:  "database_error",
+			Err:   app.NewDatabaseError(errors.New("failed to connect")),
+			IsErr: app.IsDatabaseError,
 		},
 		{
-			Name:      "another_error",
-			Err:       errors.New("failed to connect"),
-			IsSameErr: false,
+			Name:     "NON_database_error",
+			Err:      errors.New("failed to connect"),
+			IsErr:    app.IsDatabaseError,
+			Reversed: true,
+		},
+		{
+			Name:  "test_campaign_not_found_error",
+			Err:   app.NewTestCampaignNotFoundError(errors.New("no documents")),
+			IsErr: app.IsTestCampaignNotFoundError,
+		},
+		{
+			Name:     "NON_test_campaign_not_found_error",
+			Err:      errors.New("no documents"),
+			IsErr:    app.IsTestCampaignNotFoundError,
+			Reversed: true,
+		},
+		{
+			Name:  "specification_not_found_error",
+			Err:   app.NewSpecificationNotFoundError(errors.New("no documents")),
+			IsErr: app.IsSpecificationNotFoundError,
+		},
+		{
+			Name:     "NON_specification_not_found_error",
+			Err:      errors.New("no documents"),
+			IsErr:    app.IsSpecificationNotFoundError,
+			Reversed: true,
+		},
+		{
+			Name:  "performance_not_found_error",
+			Err:   app.NewPerformanceNotFoundError(errors.New("no documents")),
+			IsErr: app.IsPerformanceNotFoundError,
+		},
+		{
+			Name:     "NON_performance_not_found_error",
+			Err:      errors.New("no documents"),
+			IsErr:    app.IsPerformanceNotFoundError,
+			Reversed: true,
+		},
+		{
+			Name:  "flow_not_found_error",
+			Err:   app.NewFlowNotFoundError(errors.New("no documents")),
+			IsErr: app.IsFlowNotFoundError,
+		},
+		{
+			Name:     "NON_flow_not_found_error",
+			Err:      errors.New("no documents"),
+			IsErr:    app.IsFlowNotFoundError,
+			Reversed: true,
+		},
+		{
+			Name:  "already_exists_error",
+			Err:   app.NewAlreadyExistsError(errors.New("duplicate key")),
+			IsErr: app.IsAlreadyExistsError,
+		},
+		{
+			Name:     "NON_already_exists_error",
+			Err:      errors.New("duplicate key"),
+			IsErr:    app.IsAlreadyExistsError,
+			Reversed: true,
 		},
 	}
 
@@ -35,162 +92,13 @@ func TestIsDatabaseError(t *testing.T) {
 		t.Run(c.Name, func(t *testing.T) {
 			t.Parallel()
 
-			require.Equal(t, c.IsSameErr, app.IsDatabaseError(c.Err))
-		})
-	}
-}
+			if c.Reversed {
+				require.False(t, c.IsErr(c.Err))
 
-func TestIsTestCampaignNotFoundError(t *testing.T) {
-	t.Parallel()
+				return
+			}
 
-	testCases := []struct {
-		Name      string
-		Err       error
-		IsSameErr bool
-	}{
-		{
-			Name:      "test_campaign_not_found_error",
-			Err:       app.NewTestCampaignNotFoundError(errors.New("no documents")),
-			IsSameErr: true,
-		},
-		{
-			Name:      "another_error",
-			Err:       errors.New("no documents"),
-			IsSameErr: false,
-		},
-	}
-
-	for _, c := range testCases {
-		c := c
-
-		t.Run(c.Name, func(t *testing.T) {
-			t.Parallel()
-
-			require.Equal(t, c.IsSameErr, app.IsTestCampaignNotFoundError(c.Err))
-		})
-	}
-}
-
-func TestIsSpecificationNotFoundError(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		Name      string
-		Err       error
-		IsSameErr bool
-	}{
-		{
-			Name:      "specification_not_found_error",
-			Err:       app.NewSpecificationNotFoundError(errors.New("no documents")),
-			IsSameErr: true,
-		},
-		{
-			Name:      "another_error",
-			Err:       errors.New("no documents"),
-			IsSameErr: false,
-		},
-	}
-
-	for _, c := range testCases {
-		c := c
-
-		t.Run(c.Name, func(t *testing.T) {
-			t.Parallel()
-
-			require.Equal(t, c.IsSameErr, app.IsSpecificationNotFoundError(c.Err))
-		})
-	}
-}
-
-func TestIsPerformanceNotFoundError(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		Name      string
-		Err       error
-		IsSameErr bool
-	}{
-		{
-			Name:      "performance_not_found_error",
-			Err:       app.NewPerformanceNotFoundError(errors.New("no documents")),
-			IsSameErr: true,
-		},
-		{
-			Name:      "another_error",
-			Err:       errors.New("no documents"),
-			IsSameErr: false,
-		},
-	}
-
-	for _, c := range testCases {
-		c := c
-
-		t.Run(c.Name, func(t *testing.T) {
-			t.Parallel()
-
-			require.Equal(t, c.IsSameErr, app.IsPerformanceNotFoundError(c.Err))
-		})
-	}
-}
-
-func TestIsFlowNotFoundError(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		Name      string
-		Err       error
-		IsSameErr bool
-	}{
-		{
-			Name:      "flow_not_found_error",
-			Err:       app.NewFlowNotFoundError(errors.New("no documents")),
-			IsSameErr: true,
-		},
-		{
-			Name:      "another_error",
-			Err:       errors.New("no documents"),
-			IsSameErr: false,
-		},
-	}
-
-	for _, c := range testCases {
-		c := c
-
-		t.Run(c.Name, func(t *testing.T) {
-			t.Parallel()
-
-			require.Equal(t, c.IsSameErr, app.IsFlowNotFoundError(c.Err))
-		})
-	}
-}
-
-func TestIsAlreadyExistsError(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		Name      string
-		Err       error
-		IsSameErr bool
-	}{
-		{
-			Name:      "already_exists_error",
-			Err:       app.NewAlreadyExistsError(errors.New("duplicate key")),
-			IsSameErr: true,
-		},
-		{
-			Name:      "another_error",
-			Err:       errors.New("duplicate key"),
-			IsSameErr: false,
-		},
-	}
-
-	for _, c := range testCases {
-		c := c
-
-		t.Run(c.Name, func(t *testing.T) {
-			t.Parallel()
-
-			require.Equal(t, c.IsSameErr, app.IsAlreadyExistsError(c.Err))
+			require.True(t, c.IsErr(c.Err))
 		})
 	}
 }
