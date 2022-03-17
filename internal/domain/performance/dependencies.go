d8e51a1692e6bc56a3455636405e4478a34ff609
Djerys <kotukhov99@gmail.com>
2022-03-17 16:57:38 +0300

    Add docs for `dependencies` (#68)

--- internal/domain/performance/dependencies.go
+++ internal/domain/performance/dependencies.go
@@ -7,6 +7,14 @@ import (
 )
 
 type (
+	// ScenarioSyncGroup syncs the performing of theses.
+	// SyncDependencies collects all dependencies of scenario
+	// into a dependency graph.
+	//
+	// Each performing thesis goroutine receives a ScenarioSyncGroup
+	// and calls WaitThesisDependencies at the beginning thesis
+	// performing. Then each thesis goroutine calls ThesisDone
+	// when finished.
 	ScenarioSyncGroup struct {
 		slug   specification.Slug
 		theses map[specification.Slug]thesisSync
@@ -18,6 +26,14 @@ type (
 	}
 )
 
+// SyncDependencies collects dependencies within specification.Scenario
+// into one ScenarioSyncGroup, with which will be possible to manage and
+// synchronize the performing of theses, taking into account the
+// dependencies of each thesis.
+//
+// This can be used inside the performance to control the execution of
+// theses, preventing the thesis from being performed if the theses
+// dependent on it are not performed.
 func SyncDependencies(scenario specification.Scenario) ScenarioSyncGroup {
 	theses := scenario.Theses()
 
@@ -102,6 +118,8 @@ func slugsSet(slugs []specification.Slug) map[specification.Slug]bool {
 	return set
 }
 
+// Snapshot returns a map representation of dependencies
+// inside the scenario.
 func (g ScenarioSyncGroup) Snapshot() DependenciesSnapshot {
 	if g.theses == nil || len(g.theses) == 0 {
 		return nil
@@ -121,10 +139,17 @@ func (g ScenarioSyncGroup) Snapshot() DependenciesSnapshot {
 	return snp
 }
 
+// Slug returns the slug of the scenario for which
+// dependencies are collected.
 func (g ScenarioSyncGroup) Slug() specification.Slug {
 	return g.slug
 }
 
+// WaitThesisDependencies blocks goroutine until all
+// thesis dependencies have finished.
+//
+// You must pass the thesis slug, the dependencies
+// of which you need to wait for.
 func (g ScenarioSyncGroup) WaitThesisDependencies(
 	ctx context.Context,
 	slug specification.Slug,
@@ -145,6 +170,8 @@ func (g ScenarioSyncGroup) WaitThesisDependencies(
 	return nil
 }
 
+// ThesisDone notifies all pending theses that the thesis
+// with the passed slug are finished.
 func (g ScenarioSyncGroup) ThesisDone(slug specification.Slug) {
 	if thesis, ok := g.theses[slug]; ok {
 		close(thesis.done)
