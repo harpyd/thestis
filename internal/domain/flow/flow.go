97adb2ea7b948041ecd296984143af6c59d890ac
Djerys <kotukhov99@gmail.com>
2022-03-17 09:46:05 +0300

    Add docs for public functions and structs in `flow` (#68)

--- internal/domain/flow/flow.go
+++ internal/domain/flow/flow.go
@@ -6,9 +6,9 @@ import (
 )
 
 type (
-	// Flow represents current performance.Performance performing.
-	// Flow keeps transitions information
-	// and common state of performing.
+	// Flow represents the progress of a single performance.Performance
+	// run. The flow consists of working specification.Scenario and
+	// specification.Thesis statuses.
 	Flow struct {
 		id            string
 		performanceID string
@@ -16,6 +16,8 @@ type (
 		statuses []Status
 	}
 
+	// Status represents progress of the slugged object like
+	// specification.Scenario and specification.Thesis.
 	Status struct {
 		slug         specification.Slug
 		state        State
@@ -32,14 +34,18 @@ type (
 	}
 )
 
+// ID returns flow identifier.
 func (f Flow) ID() string {
 	return f.id
 }
 
+// PerformanceID returns associated with
+// Flow performance identifier.
 func (f Flow) PerformanceID() string {
 	return f.performanceID
 }
 
+// Statuses returns copy of slugged object statuses.
 func (f Flow) Statuses() []Status {
 	statuses := make([]Status, len(f.statuses))
 	copy(statuses, f.statuses)
@@ -47,6 +53,7 @@ func (f Flow) Statuses() []Status {
 	return statuses
 }
 
+// NewStatus creates representation of slugged object progress.
 func NewStatus(slug specification.Slug, state State, errMessages ...string) Status {
 	return Status{
 		slug:         slug,
@@ -63,14 +70,18 @@ func errorsOrNil(errs []string) []string {
 	return errs
 }
 
+// Slug returns associated with Status object slug.
 func (f Status) Slug() specification.Slug {
 	return f.slug
 }
 
+// State returns state of slugged object.
 func (f Status) State() State {
 	return f.state
 }
 
+// OccurredErrs returns errors that occurred
+// during performing of slugged objects.
 func (f Status) OccurredErrs() []string {
 	occurredErrs := make([]string, len(f.occurredErrs))
 	copy(occurredErrs, f.occurredErrs)
@@ -84,6 +95,13 @@ type Params struct {
 	Statuses      []Status
 }
 
+// Unmarshal transforms Params to Flow.
+//
+// This function is great for converting
+// from a database or using in tests.
+//
+// You must not use this method in
+// business code of domain and app layers.
 func Unmarshal(params Params) Flow {
 	f := Flow{
 		id:            params.ID,
@@ -96,6 +114,16 @@ func Unmarshal(params Params) Flow {
 	return f
 }
 
+// FromPerformance starts a new flow from performance.Performance.
+// The result of the function is a Reducer, with which you can
+// collect the steps coming from performance.Performance
+// during its execution.
+//
+// Each step contains information about the progress of the
+// performance.Performance, including performance.Event.
+// The states of statuses change under the action of events.
+// The transition rules for the event are described in the
+// State.Next method.
 func FromPerformance(id string, perf *performance.Performance) *Reducer {
 	scenarios := perf.WorkingScenarios()
 
@@ -122,6 +150,10 @@ func FromPerformance(id string, perf *performance.Performance) *Reducer {
 	}
 }
 
+// FromStatuses start a new flow from initialized statuses.
+//
+// This method is similar to the other, but this method is
+// intended solely for testing purposes.
 func FromStatuses(id, performanceID string, statuses ...Status) *Reducer {
 	r := &Reducer{
 		id:            id,
@@ -140,9 +172,9 @@ func FromStatuses(id, performanceID string, statuses ...Status) *Reducer {
 	return r
 }
 
-// Reduce creates current version of Flow from FlowReducer.
-// This is useful for accumulating Performance Step's and
-// storing the state of Performance Flow.
+// Reduce creates current version of Flow from Reducer.
+// This is useful for accumulating performance.Performance steps and
+// storing the state of performance.Performance's Flow.
 //
 // For example:
 //  fr := performance.FlowFromPerformance("id", perf)
@@ -169,11 +201,12 @@ func (r *Reducer) statusesSnapshot() []Status {
 	return statuses
 }
 
-// WithStep is method for step by step collecting Step's for
-// their further reduction with FlowReducer's Reduce.
+// WithStep is method for step by step collecting
+// performance.Performance steps for their further
+// reduction with Reducer's Reduce.
 //
 // Flow state changes from call to call relying on the
-// state transition rules.
+// state transition rules in State.Next method.
 func (r *Reducer) WithStep(step performance.Step) *Reducer {
 	slug := step.Slug()
 
