57e3dbbb931db89f352ce95948adabe318e27fdc
Djerys <kotukhov99@gmail.com>
2022-03-26 15:54:49 +0300

    Combine files

--- internal/port/http/auth/provider.go
+++ internal/port/http/auth/provider.go
@@ -3,8 +3,30 @@ package auth
 import (
 	"context"
 	"net/http"
+
+	"github.com/pkg/errors"
 )
 
 type Provider interface {
 	AuthenticatedUser(ctx context.Context, r *http.Request) (User, error)
+}
+
+type User struct {
+	UUID        string
+	DisplayName string
+}
+
+type ctxKey int
+
+const userCtxKey ctxKey = iota
+
+var errNoUserInCtx = errors.New("no user in context")
+
+func UserFromCtx(ctx context.Context) (User, error) {
+	u, ok := ctx.Value(userCtxKey).(User)
+	if !ok {
+		return User{}, errNoUserInCtx
+	}
+
+	return u, nil
 }
