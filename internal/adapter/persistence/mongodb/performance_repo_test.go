50d417539c3db05d21928ee13ada5bbd9bd7f085
Djerys <kotukhov99@gmail.com>
2022-04-19 13:48:29 +0300

    Refactor `performance_repo_test` #81

--- internal/adapter/persistence/mongodb/performance_repo_test.go
+++ internal/adapter/persistence/mongodb/performance_repo_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/harpyd/thestis/internal/adapter/persistence/mongodb"
@@ -41,29 +42,29 @@ func TestPerformanceRepository(t *testing.T) {
 }
 
 func (s *PerformanceRepositoryTestSuite) TestAddPerformance() {
+	var b specification.Builder
+
+	availableSpec := b.
+		WithID("1a63b6ea-df5f-4a68-bf04-c2e30044f2ef").
+		ErrlessBuild()
+
 	testCases := []struct {
-		Name                   string
-		Before                 func()
-		GivenPerformanceParams performance.Params
-		ShouldBeErr            bool
-		IsErr                  func(err error) bool
+		Name                      string
+		InsertedBeforePerformance interface{}
+		GivenPerformance          *performance.Performance
+		ShouldBeErr               bool
+		IsErr                     func(err error) bool
 	}{
 		{
 			Name: "failed_adding_one_performance_twice",
-			Before: func() {
-				perf := performance.Unmarshal(performance.Params{
-					ID: "a4a2906d-4df5-42f1-8832-77a33cba4d7f",
-				})
-
-				s.addPerformances(perf)
+			InsertedBeforePerformance: bson.M{
+				"_id": "a4a2906d-4df5-42f1-8832-77a33cba4d7f",
 			},
-			GivenPerformanceParams: performance.Params{
-				ID:      "a4a2906d-4df5-42f1-8832-77a33cba4d7f",
-				OwnerID: "05bf69e9-d7b5-4e7a-8fab-24227dca033a",
-				Specification: (&specification.Builder{}).
-					WithID("1a63b6ea-df5f-4a68-bf04-c2e30044f2ef").
-					ErrlessBuild(),
-			},
+			GivenPerformance: performance.Unmarshal(performance.Params{
+				ID:            "a4a2906d-4df5-42f1-8832-77a33cba4d7f",
+				OwnerID:       "05bf69e9-d7b5-4e7a-8fab-24227dca033a",
+				Specification: availableSpec,
+			}),
 			ShouldBeErr: true,
 			IsErr: func(err error) bool {
 				var target *app.DatabaseError
@@ -74,27 +75,23 @@ func (s *PerformanceRepositoryTestSuite) TestAddPerformance() {
 		},
 		{
 			Name: "successful_adding",
-			GivenPerformanceParams: performance.Params{
-				ID:      "3ce098e1-81ae-4610-8372-2f635b1b6a0c",
-				OwnerID: "3614a95c-c278-4687-84e2-97b95b11d399",
-				Specification: (&specification.Builder{}).
-					WithID("spec").
-					ErrlessBuild(),
-				Started: true,
-			},
+			GivenPerformance: performance.Unmarshal(performance.Params{
+				ID:            "3ce098e1-81ae-4610-8372-2f635b1b6a0c",
+				OwnerID:       "3614a95c-c278-4687-84e2-97b95b11d399",
+				Specification: availableSpec,
+				Started:       true,
+			}),
 			ShouldBeErr: false,
 		},
 	}
 
 	for _, c := range testCases {
 		s.Run(c.Name, func() {
-			if c.Before != nil {
-				c.Before()
+			if c.InsertedBeforePerformance != nil {
+				s.addPerformances(c.InsertedBeforePerformance)
 			}
 
-			perf := performance.Unmarshal(c.GivenPerformanceParams)
-
-			err := s.repo.AddPerformance(context.Background(), perf)
+			err := s.repo.AddPerformance(context.Background(), c.GivenPerformance)
 
 			if c.ShouldBeErr {
 				s.Require().True(c.IsErr(err))
@@ -106,23 +103,23 @@ func (s *PerformanceRepositoryTestSuite) TestAddPerformance() {
 
 			persistedPerf, err := s.repo.GetPerformance(
 				context.Background(),
-				perf.ID(),
-				app.AvailableSpecification(c.GivenPerformanceParams.Specification),
+				c.GivenPerformance.ID(),
+				app.AvailableSpecification(availableSpec),
 			)
 			s.Require().NoError(err)
 
-			s.Require().Equal(perf, persistedPerf)
+			s.Require().Equal(c.GivenPerformance, persistedPerf)
 		})
 	}
 }
 
-func (s *PerformanceRepositoryTestSuite) addPerformances(perfs ...*performance.Performance) {
+func (s *PerformanceRepositoryTestSuite) addPerformances(perfs ...interface{}) {
 	s.T().Helper()
 
 	ctx := context.Background()
 
 	for _, perf := range perfs {
-		err := s.repo.AddPerformance(ctx, perf)
+		_, err := s.db.Collection("performances").InsertOne(ctx, perf)
 		s.Require().NoError(err)
 	}
 }
